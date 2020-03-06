25a15ec24dee2245167e3317f2d8e38858dd85b8
Ferres Enzo <Enzo@MacBook-Pro-de-Ferres.local>
2020-03-06 17:10:09 +0100

    Modal suppression chapitre

--- view/backend/sbadmin2/listingpost.php
+++ view/backend/sbadmin2/listingpost.php
@@ -26,7 +26,35 @@
           <td><?= substr(nl2br(htmlspecialchars($posts[$i]['content'])), 0, 100) ?> [...]</td>
           <td><?= $posts[$i]['creation_date_fr'] ?></td>
           <td><a href="index.php?action=editpost&id=<?= $posts[$i]['id'] ?>" class="btn btn-warning">Modifier</a> 
-          <a href="index.php?action=deletepost&id=<?= $posts[$i]['id'] ?>" class="btn btn-danger">Supprimer</a></td>                   
+
+          <!-- <a href="index.php?action=deletepost&id=<?= $posts[$i]['id'] ?>" class="btn btn-danger">Supprimer</a> -->
+
+          <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#deletepost<?= $posts[$i]['id'] ?>">
+            Supprimer le chapitre
+          </button>
+
+          <!-- Modal -->
+          <div class="modal fade" id="deletepost<?= $posts[$i]['id'] ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                 <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Vous êtes sûr(e) ?</h5>
+                   <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                   </button>
+                </div>
+                <div class="modal-body">
+                  Ce commentaire sera supprimé et n'apparaitre plus sur le site
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
+                  <a href="index.php?action=deletepost&id=<?= $posts[$i]['id'] ?>" class="btn btn-danger">Supprimer ce chapitre</a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          </td>                   
         </tr> 
       <?php endfor; ?>     
       </tbody>
