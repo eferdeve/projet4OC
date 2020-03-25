679f616ade4c515b664c108a545395d994eb2dd6
Ferres Enzo <Enzo@MacBook-Pro-de-Ferres.local>
2020-03-25 13:26:47 +0100

    -

--- public/main.js
+++ public/main.js
@@ -1,3 +1,2 @@
 //Mode nuit
 let nightmode = new NightMode();
-
---
+++ public/warningcom.js
@@ -0,0 +1,11 @@
+//Warning Mode
+class Warning {
+    constructor() {
+
+    }
+    Hidenbox() {
+        $('#warn').click(function() {
+            $('#combox').html('Ce commentaire à été signalé');
+        })
+    }
+}
